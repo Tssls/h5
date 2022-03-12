d0bc0e29eb34203f2a828cbd0db9d4b58ffc0ff9
Ts <2539626150@qq.com>
2022-03-12 18:05:51 +0800

    'fix:Dockerfile'

--- src/Home/index.jsx
+++ src/Home/index.jsx
@@ -25,6 +25,30 @@ function Home() {
   }
 
   useEffect(() => {
+    window.jWeixin.config({
+      debug: true, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
+      appId: 'wxe73752be84bedd52', // 必填，公众号的唯一标识
+      timestamp: '1', // 必填，生成签名的时间戳
+      nonceStr: '2', // 必填，生成签名的随机串
+      signature: '3',
+      jsApiList: ['getLocation'] // 必填，需要使用的JS接口列表
+    });
+    window.jWeixin.ready(function () {
+      window.jWeixin.getLocation({
+        type: 'wgs84', // 默认为wgs84的gps坐标，如果要返回直接给openLocation用的火星坐标，可传入'gcj02'
+        success: function (res) {
+          console.log('res', res)
+          var latitude = res.latitude; // 纬度，浮点数，范围为90 ~ -90
+          var longitude = res.longitude; // 经度，浮点数，范围为180 ~ -180。
+          var speed = res.speed; // 速度，以米/每秒计
+          var accuracy = res.accuracy; // 位置精度
+          alert(latitude)
+        },
+        fail: function (err) {
+          console.log('err', err)
+        }
+      });
+    });
     setTimeout(() => {
       const gdMap = bMapTransqqMap(window.lat, window.lng)
       const arr = distanceList.map((item) => {
